040a383d9fe16757eca4db274a0523d1fe494e3a
Prateek Singh <prateekchadha15@gmail.com>
2022-10-23 15:29:34 +0530

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,15 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Application } from './Components/Application/Application';
+import { Skills } from './Components/skills/Skills';
 
 function App() {
+  const skills = ['HTML', 'CSS', 'JavaScript']
+
   return (
     <div className="App">
    <Application />
+   <Skills skills={skills} />
     </div>
   );
 }
